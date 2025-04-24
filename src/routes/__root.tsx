96912a5560010a3821f58e9563ef7a5a09f39111
rellityuint32 <zachari.iligan@asceoft.com>
2025-04-24 09:13:49 +0800

    fix analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,31 +6,11 @@ import { SubHeader } from '@/components/user/layout/sub-header'
 
 export const Route = createRootRoute({
   component: () => (
-    <>
-      <script
-        defer
-        data-domain="balinkbayan.r3workshop.live"
-        src="https://pl-analytics.r3workshop.live/js/script.js"
-      ></script>
-      <script dangerouslySetInnerHTML={{
-        __html: `
-          window.plausible = window.plausible || function() { 
-            (window.plausible.q = window.plausible.q || []).push(arguments);
-          }
-        `
-      }}>
-      </script>
-
-      <div className='w-full overflow-none'>
-
-
-        <SubHeader />
-
-        <Outlet />
-        <TanStackRouterDevtools />
-
-        <Footer />
-      </div>
-    </>
+    <div className='w-full overflow-none'>
+      <SubHeader />
+      <Outlet />
+      <TanStackRouterDevtools />
+      <Footer />
+    </div>
   ),
 })
