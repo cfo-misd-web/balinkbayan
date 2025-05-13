cf97a3ca0571ea0bdd46ad927447ee85a0bae509
KiahRome <jzcaladiao2021@plm.edu.ph>
2025-05-13 08:16:07 +0800

    fix: contact us header

--- src/components/user/contact/contact-head.tsx
+++ src/components/user/contact/contact-head.tsx
@@ -11,7 +11,7 @@ const ContactHead = () => {
             <motion.div
                 className="w-full h-[700px] max-lg:h-[500px] relative bg-cover bg-center"
                 style={{
-                    backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.2)), url('/img-assets/img-1.jpg')`,
+                    backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.2)), url('/img-assets/contact-us/contact-us-4193523__340.jpg')`,
                 }}
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
@@ -20,7 +20,7 @@ const ContactHead = () => {
             >
                 <div className="absolute inset-0 bg-black/50"></div>
                 <div className="font-extrabold text-center text-[80px] max-lg:text-4xl text-white drop-shadow-lg absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                    Downloads
+                    Contact Us
                     <div className='flex flex-col items-center justify-center mt-4 gap-y-5'>
                         <Separator className='w-full data-[orientation=horizontal]:h-[5px] rounded-full bg-green-700' />
                         <Separator className='data-[orientation=horizontal]:w-3/4 data-[orientation=horizontal]:h-[5px] rounded-full bg-[#F4A03A]' />
