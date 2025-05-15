2f3d2da217118d4e51f7ade4697413c36a098770
KiahRome <jzcaladiao2021@plm.edu.ph>
2025-05-15 10:24:24 +0800

    fix: Headings and Paragraphs

--- src/components/user/shared/banner/header.tsx
+++ src/components/user/shared/banner/header.tsx
@@ -1,34 +1,34 @@
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
+const HeaderTemp = ({
+  label,
+  img_url,
+}: {
+  label?: string;
+  img_url: string;
+}) => {
+  return (
+    <section className="relative">
+      <motion.div
+        className="w-full h-[700px] max-lg:h-[500px] relative bg-cover bg-center"
+        style={{
+          backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.2)), url(${img_url})`,
+        }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1, ease: "easeInOut" }}>
+        <div className="absolute inset-0 bg-black/50"></div>
+      </motion.div>
 
-const HeaderTemp = ({ label , img_url }:{ label?: string, img_url:string }) => {
+      <h2 className="max-lg:hidden font-[1000] absolute text-start top-1/2 left-1/3 transform -translate-x-1/3 -translate-y-[50%] text-white text-[80px] drop-shadow-lg max-lg:w-[90%] max-lg:mx-auto">
+        {label}
+      </h2>
 
-
-    return (
-        <section className="relative">
-
-            <motion.div
-                className="w-full h-[700px] max-lg:h-[500px] relative bg-cover bg-center"
-                style={{
-                    backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.2)), url(${img_url})`,
-                }}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 1, ease: 'easeInOut' }}
-            >
-                <div className="absolute inset-0 bg-black/50"></div>
-            </motion.div>
-
-            <h2 className='max-lg:hidden font-[1000] absolute text-start top-1/2 left-1/3 transform -translate-x-1/3 -translate-y-[50%] text-white text-[80px] drop-shadow-lg max-lg:w-[90%] max-lg:mx-auto'>
-                {label}
-            </h2>
-
-            <div className='hidden max-lg:block absolute text-start bottom-2/3 translate-y-28 left-1/12 font-extrabold  text-[45px] text-white gap-y-1.5'>
-                {label}
-            </div>
-
-        </section>
-    );
+      <div className="hidden max-lg:block absolute text-start bottom-2/3 translate-y-28 left-1/12 font-extrabold  text-[45px] text-white gap-y-1.5">
+        {label}
+      </div>
+    </section>
+  );
 };
 export default HeaderTemp;
--- src/components/user/shared/banner/smaller-banner.tsx
+++ src/components/user/shared/banner/smaller-banner.tsx
@@ -1,35 +1,44 @@
-import { cn } from '@/lib/utils';
-import { motion } from 'framer-motion';
+import { cn } from "@/lib/utils";
+import { motion } from "framer-motion";
 type SmallerBannerProps = {
-    label?:string,
-    img_url?:string,
-    className?:string,
-    textClass?:string,
-    divClass?:string
-}
+  label?: string;
+  img_url?: string;
+  className?: string;
+  textClass?: string;
+  divClass?: string;
+};
 
-const SmallerBanner = ({label, img_url, className, textClass, divClass}: SmallerBannerProps) => {
-    return (
-        <section className="relative overflow-hidden">
-            <motion.div
-                className={cn("w-full h-[150px] relative bg-cover bg-center", className)}
-                style={{
-                    backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.2)), url(${img_url})`,
-                    filter: 'blur(2px) brightness(0.8)',
-                }}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 1, ease: 'easeInOut' }}
-            >
-            </motion.div>
+const SmallerBanner = ({
+  label,
+  img_url,
+  className,
+  textClass,
+  divClass,
+}: SmallerBannerProps) => {
+  return (
+    <section className="relative overflow-hidden">
+      <motion.div
+        className={cn(
+          "w-full h-[150px] relative bg-cover bg-center",
+          className
+        )}
+        style={{
+          backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.2)), url(${img_url})`,
+          filter: "blur(2px) brightness(0.8)",
+        }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1, ease: "easeInOut" }}></motion.div>
 
-            <div className={cn('flex w-full justify-center text-center items-center text-white absolute bottom-2', divClass)}>
-                <h2 className={cn('text-9xl font-black', textClass)}>
-                    {label}
-                </h2>
-            </div>
-        </section>
-    );
+      <div
+        className={cn(
+          "flex w-full justify-center text-center items-center text-white absolute bottom-2",
+          divClass
+        )}>
+        <p className={cn("text-9xl font-black", textClass)}>{label}</p>
+      </div>
+    </section>
+  );
 };
 export default SmallerBanner;
